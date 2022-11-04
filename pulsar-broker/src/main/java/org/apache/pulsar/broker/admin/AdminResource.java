ec467257f5225356cc6ac9f71786111cc71e0e1e
huangzegui <huangzegui@didiglobal.com>
2022-11-04 15:36:26 +0800

    test

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/AdminResource.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/AdminResource.java
@@ -648,11 +648,7 @@ public abstract class AdminResource extends PulsarWebResource {
                 })
                 .exceptionally(ex -> {
                     log.error("[{}] Failed to create partitions for topic {}", clientAppId(), topicName, ex);
-                    if (ex instanceof RestException) {
-                        asyncResponse.resume(ex);
-                    } else {
-                        resumeAsyncResponseExceptionally(asyncResponse, ex);
-                    }
+                    resumeAsyncResponseExceptionally(asyncResponse, ex);
                     return null;
                 });
     }
